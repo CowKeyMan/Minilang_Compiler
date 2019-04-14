7ae4155576f3c064a76a30a147629cff9b209f9d
DANIEL\Daniel Cauchi <daniel.cauchi.17@um.edu.mt>
2019-04-14 11:53:16 +0200

    Continued Parser

--- code/src/Parser/AST.cpp
+++ code/src/Parser/AST.cpp
@@ -1,32 +1,62 @@
+#include <iostream>
+
 #include "AST.h"
 
-bool ASTNode::parseOptional(){
-  if(tokenManager->peekToken() == nullptr) return false;
+using std::cerr;
 
-  // is the start tokens are empty (any token goes), no need to check
-  if(startTokens().size() == 0){
-    parse();
-    return true;
+// ASTNode *ASTNode::parseOptional(){
+//   if(tokenManager->peekToken() == nullptr) return NULL;
+
+//   // is the start tokens are empty (any token goes), no need to check
+//   if(startTokens().size() == 0){
+//     parse();
+//     return NULL;
+//   }
+
+//   TokenType tt = tokenManager->peekToken()->type;
+//   for(int i = 0; i < startTokens().size(); ++i){
+//     if(tt = startTokens()[i]){ // check if can be parsed
+//       parse(); 
+//       return true;
+//     }
+//   }
+//   return NULL;
+// }
+
+// vector<ASTNode*> ASTNode::parseMultiple(){
+//   if(tokenManager->peekToken() == nullptr) return;
+
+//   bool cont;
+//   TokenType tt = tokenManager->peekToken()->type;
+//   do{
+//     cont = parseOptional();
+//   }while(cont); // do it until it's no longer possible
+// }
+
+void ASTNode::match(TokenType tt){
+  Token *current = tokenManager->nextToken();
+  if(current->type != tt){
+    cerr << "error, could now match " << Token::TokenString[tt] << "at line " << current->lineNumber;
+  }
+}
+
+// Program Node
+vector<TokenType> ASTNodeProgram::_startTokens = vector<TokenType>(); // empty
+bool ASTNodeProgram::parse(){
+  while(true){ // parse multiple statements
+    if (tokenManager->peekToken() == nullptr) break;
+    
+    ASTNode *statement = new ASTNodeStatement(tokenManager);
+    if (statement->parse() == false) return false; // statement must be parsable if a token exists
+    statements.push_back(statement);
   }
 
-  TokenType tt = tokenManager->peekToken()->type;
-  for(int i = 0; i < startTokens().size(); ++i){
-    if(tt = startTokens()[i]){ // check if can be parsed
-      parse(); 
-      return true;
-    }
-  }
+  if (statements.size() > 0) return true; // needs to be at least one statement
   return false;
 }
 
-void ASTNode::parseMultiple(){
-  if(tokenManager->peekToken() == nullptr) return;
-
-  bool cont;
-  TokenType tt = tokenManager->peekToken()->type;
-  do{
-    cont = parseOptional();
-  }while(cont); // do it until it's no longer possible
+// Statement Node
+vector<TokenType> ASTNodeStatement::_startTokens = {IF};
+bool ASTNodeStatement::parse(){
+  return false;
 }
-
-vector<TokenType> ASTNodeProgram::_startTokens = vector<TokenType>(); // empty
--- code/src/Parser/AST.h
+++ code/src/Parser/AST.h
@@ -10,9 +10,9 @@ class ASTNode{
   public:
     ASTNode(shared_ptr<TokenManager> _tokenManager){ tokenManager = _tokenManager; };
     virtual ~ASTNode(void){};
-    virtual void parse() = 0;
-    bool parseOptional();
-    void parseMultiple();
+    virtual bool parse() = 0; // returns true if parse was successful
+    // ASTNode *parseOptional();
+    // vector<ASTNode*> parseMultiple();
   protected:
     shared_ptr<TokenManager> tokenManager;
     void match(TokenType tokenType); // match a TokenType, make sure it exists
@@ -21,24 +21,41 @@ class ASTNode{
 
 class ASTNodeProgram : virtual public ASTNode{
   public:
+    // costructor is same as parent
     ASTNodeProgram(shared_ptr<TokenManager> tokenManger) : ASTNode(tokenManager) {};
     virtual ~ASTNodeProgram(void){};
-    virtual void parse(){};
+    virtual bool parse(); // returns true if parse was successful
   protected:
     vector<TokenType> startTokens(){ return _startTokens; };
   private:
-    vector<ASTNode*> statements; // statement node
+    vector<ASTNode*> statements; // statement nodes (multiple)
     static vector<TokenType> _startTokens;
 };
 
-// class ASTNodeStatement : virtual public ASTNode{
-//   public:
-//     ASTNodeStatement(shared_ptr<TokenManager> tokenManger) : ASTNode(tokenManager) {};
-//     void parse();
-//   private:
-//     // VariableDecl/Assignemnt/PrintStatemenet/
-//     // IfStatement/ForStatementReturnStatement/FunctionDecl/Block
-//     ASTNode* node; 
-// };
+class ASTNodeStatement : virtual public ASTNode{
+  public:
+    // costructor is same as parent
+    ASTNodeStatement(shared_ptr<TokenManager> tokenManger) : ASTNode(tokenManager) {};
+    virtual ~ASTNodeStatement(void){};
+    virtual bool parse(); // returns true if parse was successful
+  protected:
+    vector<TokenType> startTokens(){ return _startTokens; };
+  private:
+    ASTNode* statement; // statement node
+    static vector<TokenType> _startTokens;
+};
+
+class ASTNodeVariableDecl : virtual public ASTNode{
+  public:
+    // costructor is same as parent
+    ASTNodeVariableDecl(shared_ptr<TokenManager> tokenManger) : ASTNode(tokenManager) {};
+    virtual ~ASTNodeVariableDecl(void){};
+    virtual bool parse(); // returns true if parse was successful
+  protected:
+    vector<TokenType> startTokens(){ return _startTokens; };
+  private:
+    ASTNode* statement; // statement node
+    static vector<TokenType> _startTokens;
+};
 
 #endif // ASTNODE_H
